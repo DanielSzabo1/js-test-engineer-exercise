61990bb86352f89184b099a7ed7df06d2e55e6c5
DanielSzabo1 <daniel_szabo1@epam.com>
2022-07-14 09:21:21 +0200

    test cases for length of password requirement and uppercase requirement

--- cypress/e2e/integration/buyABTMonhtlyPass.cy.js
+++ cypress/e2e/integration/buyABTMonhtlyPass.cy.js
@@ -1,4 +1,6 @@
 import RegistrationPage from "../../support/pages/RegistrationPage"
+const randomEmail = require('email-generator');
+const passwordGenerator = require('generate-password');
 
 describe('Creating an account - password and email address requirements', () => {
 
@@ -8,18 +10,30 @@ describe('Creating an account - password and email address requirements', () => 
     registrationPage.acceptCookies()
   })
 
-  it('Should reject to continue with a password less than 8 characters', () => {
+  it.skip('Should reject to continue with a password less than 8 characters', () => {
+    const userEmail = randomEmail()
+    const userPassword = passwordGenerator.generate({length: 7, numbers: true}) 
+
+    registrationPage.fillOutFormThenContinue(userEmail, userPassword)
+
+    registrationPage.validateRequirement('characterNumber', false)
   })
 
   it('Should reject to continue with a password without uppercase letter', () => {
+    const userEmail = randomEmail.generateEmail()
+    const userPassword = passwordGenerator.generate({numbers: true, strict: true}).toLowerCase()
+
+    registrationPage.fillOutFormThenContinue(userEmail, userPassword)
+
+    registrationPage.validatePasswordRequirement('caseSensitivity', false)
   })
 
-  it('Should reject to continue with a password without lowercase letter', () => {
+  it.skip('Should reject to continue with a password without lowercase letter', () => {
   })
 
-  it('Should reject to continue with a password without number', () => {
+  it.skip('Should reject to continue with a password without number', () => {
   })
 
-  it('Should reject to continue with an email including not allowed characters', () => {
+  it.skip('Should reject to continue with an email including not allowed characters', () => {
   })
 })
