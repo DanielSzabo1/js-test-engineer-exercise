ec022559f9c5a3883655520b8ae1f93c377c185b
DanielSzabo1 <daniel_szabo1@epam.com>
2022-07-14 15:29:33 +0200

    Full flow for creating an account until step 3
    updating password and email generation

--- cypress/e2e/integration/buyABTMonhtlyPass.cy.js
+++ cypress/e2e/integration/buyABTMonhtlyPass.cy.js
@@ -1,27 +1,29 @@
 import RegistrationPage from "../../support/pages/RegistrationPage"
-const randomEmail = require('email-generator');
-const passwordGenerator = require('generate-password');
+import YourDetailsPage from "../../support/pages/YourDetailsPage"
+const randomNameGenerator = require('random-fullName');
+import { randomDate, generateRandomPassword, generateRandomEmail} from '../../support/utils'
 
 describe('Creating an account - password and email address requirements', () => {
-
   const registrationPage = new RegistrationPage()
+
   beforeEach(() => {
     cy.visit(Cypress.env('createAnAccountUrl'))
     registrationPage.acceptCookies()
+    registrationPage.waitForHeader('Create an account')
   })
 
   it('Should reject to continue with a password less than 8 characters', () => {
-    const userEmail = randomEmail().replaceAll('"','')
-    const userPassword = passwordGenerator.generate({length: 7, numbers: true}) 
+    const userEmail = generateRandomEmail()
+    const userPassword = generateRandomPassword(/*hasNumber*/ true, /*hasUpperCase*/ true, /*hasLowerCase*/ true, /*length*/ 7)
 
     registrationPage.fillOutFormThenContinue(userEmail, userPassword)
 
-    registrationPage.validateRequirement('characterNumber', false)
+    registrationPage.validatePasswordRequirement('characterNumber', false)
   })
 
   it('Should reject to continue with a password without uppercase letter', () => {
-    const userEmail = randomEmail.generateEmail().replaceAll('"','')
-    const userPassword = passwordGenerator.generate({numbers: true, strict: true}).toLowerCase()
+    const userEmail = generateRandomEmail()
+    const userPassword = generateRandomPassword(/*hasNumber*/ true, /*hasUpperCase*/ true, /*hasLowerCase*/ false)
 
     registrationPage.fillOutFormThenContinue(userEmail, userPassword)
 
@@ -29,8 +31,8 @@ describe('Creating an account - password and email address requirements', () => 
   })
 
   it('Should reject to continue with a password without lowercase letter', () => {
-    const userEmail = randomEmail.generateEmail().replaceAll('"','')
-    const userPassword = passwordGenerator.generate({numbers: true, strict: true}).toUpperCase()
+    const userEmail = generateRandomEmail()
+    const userPassword = generateRandomPassword(/*hasNumber*/ true, /*hasUpperCase*/ false, /*hasLowerCase*/ true)
 
     registrationPage.fillOutFormThenContinue(userEmail, userPassword)
 
@@ -38,8 +40,8 @@ describe('Creating an account - password and email address requirements', () => 
   })
 
   it('Should reject to continue with a password without number', () => {
-    const userEmail = randomEmail.generateEmail().replaceAll('"','')
-    const userPassword = passwordGenerator.generate({strict: true})
+    const userEmail = generateRandomEmail()
+    const userPassword = generateRandomPassword(/*hasNumber*/ false, /*hasUpperCase*/ true, /*hasLowerCase*/ true)
 
     registrationPage.fillOutFormThenContinue(userEmail, userPassword)
 
@@ -47,14 +49,39 @@ describe('Creating an account - password and email address requirements', () => 
   })
 
   it('Should reject to continue with an email including not allowed characters', () => {
-    const userEmail = randomEmail.generateEmail() + 'á'
-    const userPassword = passwordGenerator.generate({numbers: true, strict: true})
+    const userEmail = generateRandomEmail('á')
+    const userPassword = generateRandomPassword(/*hasNumber*/ true, /*hasUpperCase*/ true, /*hasLowerCase*/ true)
+
+    registrationPage.fillOutFormThenContinue(userEmail, userPassword, 'Create an account')
+
+    registrationPage.validateEmailAddress()
+  })
+})
+
+describe('Creating an account - filling out the form then continue', () => {
+  const registrationPage = new RegistrationPage()
+  const yourDetailsPage = new YourDetailsPage()
+
+  before(() => {
+    cy.visit(Cypress.env('createAnAccountUrl'))
+    registrationPage.acceptCookies()
+  })
+
+  it('I should be able to continue after I fill out the form', () => {
+    const userEmail = generateRandomEmail()
+    const userPassword = generateRandomPassword(/*hasNumber*/ true, /*hasUpperCase*/ true, /*hasLowerCase*/ true)
+    const randomName = randomNameGenerator({gender: 'male'})
+    const MothersMaidenName = randomNameGenerator({gender: 'female'})
+    const dateOfBirth = randomDate(new Date(1980, 0, 1), new Date(2000, 0, 1))
 
     registrationPage.fillOutFormThenContinue(userEmail, userPassword)
 
-    registrationPage.validateEmailAddress()
+    registrationPage.waitForHeader('Your details')
 
+    yourDetailsPage.fillPersonalDetailsForm(randomName, dateOfBirth, MothersMaidenName)
 
+    yourDetailsPage.clickContinue('Your details')
 
+    yourDetailsPage.waitForHeader('Billing address')
   })
 })
