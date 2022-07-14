0466cfbbdaafb9b596cf61af1ceda0dc16bae32e
DanielSzabo1 <daniel_szabo1@epam.com>
2022-07-14 11:19:23 +0200

    set chromeWebSecurity to false
    It is necessary to be able to accept cookies on the create an account page

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     createAnAccountUrl:
       "https://www.btcom.digital-ref1.bt.com/sport/buy/monthly-pass/create-an-account",
   },
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
